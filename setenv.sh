92b4ed1f06d9e11b3fad9ceea5936b7e5436ec58
Thom Nichols <tmnichols@gmail.com>
2014-10-23 10:59:08 -0400

    added default setenv script

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+# run as `. ./setenv.sh` so these variable stick in your environment
+
+export TRELLO_API_KEY=''
+export TRELLO_WRITE_ACCESS_TOKEN=''
+export JIRA_USERNAME=''
+export JIRA_PASSWORD=''
+export JIRA_HOST=''
